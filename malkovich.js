7c90f67d186a14aad43eb4c52dda497a9c56ede5
Tif Slama <tif.slama@gmail.com>
2014-12-16 17:19:56 -0800

    basic text and image replacement works!

--- malkovich.js
+++ malkovich.js
@@ -16,6 +16,14 @@ $(document).ready(function(){
 
   // TARGET ANY IMAGE
   // REPLACE WITH SOME IMAGE OF MALKOVICH
+      // REPLACE IMAGE
+    // make an array of malkovich img src urls and shuffle them
+      // }
+  var allImg = $('img');
+  allImg.each(function() {
+    $('img').attr("src", "http://www.thestar.com/content/dam/thestar/news/gta/2013/06/08/actor_john_malkovich_comes_to_rescue_of_bleeding_man_in_toronto/malkovichshot.jpg");
+  });
+
 
     // REPLACE TEXT
     // find the text in every element and replace it with "Malkovich"
@@ -27,19 +35,77 @@ $(document).ready(function(){
     // pArray.replace(/anything/g, "malkovich");
     // pMal = pArray.join(" ");
 
-    // in jquery
-    // $("*").replaceWith("Malkovich");
-    // or
-    $('p,h1,h2,h3,h4,h5,h6,span').text("Malkovich");
+    // $('p,h1,h2,h3,h4,h5,h6,span')
+
+
+    // // splits along any spaces and non-letters (numbers, punctuation, symbols, etc.)
+    // var pText = $('p').text().split(/([_\W])/);
+
+
+   var allH = $('h1,h2,h3,h4,h5,h6');
+    allH.each(function() {
+      // console.log(this);
+      var wordsArr = $(this).text().split(" ");
+      // console.log(wordsArr);
+      var malkArray = wordsArr.map(function(singleWord) {
+        if (singleWord.length > 5) {
+          return "Malkovich";
+        } else if (singleWord.length <= 5 && singleWord >= 4) {
+          return "Malko";
+        } else {
+          return "Mal";
+        }
+      });
+      // console.log(malkArray);
+      $(this).text(malkArray.join(" "));
+    });
+
+
+    var allP = $('p');
+    allP.each(function() {
+      // console.log(this);
+      var wordsArr = $(this).text().split(" ");
+      // console.log(wordsArr);
+      var malkArray = wordsArr.map(function(singleWord) {
+        if (singleWord.length > 5) {
+          return "Malkovich";
+        } else if (singleWord.length <= 5 && singleWord >= 4) {
+          return "Malko";
+        } else {
+          return "Mal";
+        }
+      });
+      // console.log(malkArray);
+      $(this).text(malkArray.join(" "));
+    });
+
+
+    // console.log(allP.text().split(" "));
+    // console.log(pArray);
+    // pArray.forEach(function (pWord) {
+    //   // console.log(pWord);
+    //   var length = pWord.length;
+    //   if(length> 5) {
+    //     pWord = "hahahah";
+    //   }
+
+      // var length = pLength.length;
+      // console.log(length);
+      // if (length > 5) {
+      //   pLength = "Malkovich";
+      // }
+      // }
+      // $('p').text().join(" ") = pArray;
+      // } elsif (pLength <= 5 && pLength >= 4); {
+      //   $(this).replaceWith("Malko");
+      // } elsif (pLength <= 3); {
+      //   $(this).replaceWith("Mal");
+      // }
+
+    // });
+    // console.log(pArray);
 
 
 
-    // var replaced = $("body").html().replace(/text/g,'replace');
-    // $("body").html(replaced);
-    // $("*").html($("p").text().replace(/ /g," "));
-
-    // REPLACE IMAGE
-    // make an array of malkovich img src urls and shuffle them
-    // $("img").each.attr("src", );
 });
 
